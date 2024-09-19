21f816f0fa8e05ea9063e8d213f4dba9d02536b9
Hugo <dancinoman@gmail.com>
2024-09-19 19:17:59 -0400

    Revert "adding data visualiser project"
    This reverts commit 8d6c96255b6c3be0716ed0860b3994e0f88259b9.

--- src/config-schema.ts
+++
@@ -1,5 +0,0 @@
-import { Type, validator } from "@openmrs/esm-framework";
-
-export const configSchema = {};
-
-export type Config = {};
--- src/data-visualizer/data-visualizer.test.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-xit("shows display button", async () => {});
