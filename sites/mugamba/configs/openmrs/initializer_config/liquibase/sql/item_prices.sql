fb8286fca3ca44f247da1bc398b6c07c3c28d3c2
Olara Moses <molarag21996@gmail.com>
2024-10-03 01:53:33 +0000

    Test adding billable lab services via liquibase

---
+++ sites/mugamba/configs/openmrs/initializer_config/liquibase/sql/item_prices.sql
@@ -0,0 +1,6 @@
+insert into cashier_item_price (`item_price_id`, `service_id`, `price`, `payment_mode`, `name`, `creator`, `date_created`, `uuid`) 
+values 
+    ('21', '201', '10000.00', '1001', 'Cash (insured)', '1', '2024-10-02 10:44:34', 'ffe53c26-4d5f-4f89-bdcd-a8e2e3a379be'),
+    ('22', '201', '8000.00', '1002', 'Cash (non-insured)', '1', '2024-10-02 10:44:34', '0120691c-8b4b-4bdb-bd68-4f86e386eaba'),
+    ('23', '201', '8000.00', '1003', 'Mobile money (insured)', '1', '2024-10-02 10:44:34', 'c2d8680f-1e07-4eca-8678-4907481651ea'),
+    ('24', '201', '8000.00', '1004', 'Mobile money (non-insured)', '1', '2024-10-02 10:44:34', '1d430b37-30bc-481e-8f38-1613abb737ba');
