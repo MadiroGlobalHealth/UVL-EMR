34b02f1b2546b32a1898c23b63f6bdb2e8dd33d9
Olara Moses <molarag21996@gmail.com>
2024-10-08 02:45:07 +0000

    (config) add prices via liquibase with sql file

--- sites/mugamba/configs/openmrs/initializer_config/liquibase/sql/payment_modes.sql
+++
@@ -1,6 +0,0 @@
-insert into cashier_payment_mode (`payment_mode_id`, `name`, `creator`, `date_created`, `uuid`) 
-values 
-    ('1001', 'Cash (insured)', '1', '2024-10-02 10:44:34', '79fe5b0e-a2b7-468b-a5ed-408f0a49f8eb'),
-    ('1002', 'Cash (non-insured)', '1', '2024-10-02 10:44:34', 'a352e64a-4d7d-459e-afd0-99e48edd54ec'),
-    ('1003', 'Mobile money (insured)', '1', '2024-10-02 10:44:34', 'ad28b473-bc2a-452b-8748-fc98b7aebcf0'),
-    ('1004', 'Mobile money (non-insured)', '1', '2024-10-02 10:44:34', '3a02da2d-82b0-437f-90e4-7f15baa6ee66');
