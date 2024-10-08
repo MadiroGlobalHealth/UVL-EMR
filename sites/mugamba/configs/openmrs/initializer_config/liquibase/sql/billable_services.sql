298167491987d57805433f6e8cdc20793e15b246
Olara Moses <molarag21996@gmail.com>
2024-10-08 02:45:07 +0000

    (config) add prices via liquibase with sql file

--- sites/mugamba/configs/openmrs/initializer_config/liquibase/sql/billable_services.sql
+++
@@ -1,3 +0,0 @@
-insert into cashier_billable_service (`service_id`, `name`, `short_name`, `service_type`, `service_status`, `creator`, `date_created`, `uuid`, `concept_id`) 
-values 
-    ('201', 'Echography', 'ECH', '2774', 'ENABLED', '1', '2024-10-02 10:32:20', 'bb7a4e57-915c-4981-86ea-987b2e085829', '2001');
